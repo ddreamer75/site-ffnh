927ec739da400d10732a594a0f96e18fe155882c
Michael H. <35325905+ddreamer75@users.noreply.github.com>
2026-01-04 13:06:24 +0100

    Update site.mk

--- site.mk
+++ site.mk
@@ -4,8 +4,14 @@
 
 # Use OpenWRT Source commit 4f4cb52e24ddf4e931b783ea8e65ec5d5b530261 from 12-10-2024
 # Use Packages Source commit 3ab468be20c6c385ce06403bf1ff952a4de03565 from 12-10-2024
-DEFAULT_GLUON_RELEASE := next_1.5.6_MHE
+# DEFAULT_GLUON_RELEASE := next_1.5.6_MHE
+FFNH_SITE_DIR:=$(shell dirname $(realpath $(lastword $(MAKEFILE_LIST))))
+FFNH_SITE_VERSION_GIT := $(shell git -C $(FFNH_SITE_DIR) describe --tags --abbrev=0 | sed 's/-.*//')
 
+FFNH_SITE_VERSION_FALLBACK := next_1.5.6_MHE
+FFNH_SITE_VERSION := $(if $(FFNH_SITE_VERSION_GIT),$(FFNH_SITE_VERSION_GIT),$(FFNH_SITE_VERSION_FALLBACK))
+
+DEFAULT_GLUON_RELEASE := $(FFNH_SITE_VERSION)~$(shell date '+%Y%m%d')
 
 DEFAULT_GLUON_PRIORITY := 0
 # GLUON_DEPRECATED on "upgrade" to only generate sysupgrade images for older/bad devices. Set to "full" to generate factory images for DEPRECATED Devices
